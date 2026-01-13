859dd9f1c1e2bc729639680aed5e2ec791a24399
Mouad-MCH <mouadcharadi021@gmail.com>
2026-01-13 18:02:15 +0100

    add function add_info and Error_message

--- src/main.js
+++ src/main.js
@@ -7,11 +7,17 @@
  * DOM variable
 \ --------------------*/
 
+const inputs = document.querySelectorAll('input');
+const add_btn = document.getElementById("add_btn");
+
+const error_msg = document.getElementById("error_msg")
+
 
 /*---------------------*\
  * variable
 \ --------------------*/
 
+let data = [];
 
 
 /*---------------------*\
@@ -21,3 +27,48 @@
 /*---------------------*\
  * Functions
 \ --------------------*/
+
+console.log(inputs)
+
+function add_info() {
+  if(Error_fun()) return
+
+  let name = document.getElementById("name").value;
+  let prenome = document.getElementById("prenome").value;
+  let telephone = document.getElementById("telephone").value;
+  let email = document.getElementById("email").value;
+  let date = document.getElementById("date").value;
+  let motif = document.getElementById("motif").value;
+
+
+  data.push({prenome,name,email,telephone,date,motif})
+
+  console.log(data)
+
+}
+
+
+
+
+// function show Error
+
+function Error_fun() {
+  let isEmpty = false;
+  inputs.forEach(el => {
+    if(el.value === "") {
+      isEmpty = true
+      return;
+    }
+  })
+
+  if(isEmpty) {
+    error_msg.classList.remove("hidden");
+
+    setTimeout(() => {
+      error_msg.classList.add("hidden");
+    }, 1000)
+  }
+  return isEmpty;
+}
+
+add_btn.addEventListener("click", add_info)
